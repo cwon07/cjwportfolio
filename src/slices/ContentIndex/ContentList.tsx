4938a1741088831d2b0b5aacf7577961d50146e9
cwon07 <jumiryoo@gmail.com>
2024-07-18 15:38:03 -0700

    edited ContentList.tsx

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import Link from 'next/link'
 import React, { useRef, useState, useEffect } from 'react'
 import { asImageSrc, isFilled } from '@prismicio/client';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { MdArrowOutward } from 'react-icons/md'
 import { Content } from '@prismicio/client'
+import Link from 'next/link';
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -25,8 +25,8 @@ export default function ContentList ({
 }: ContentListProps) {
 
     const component = useRef(null)
-    const revealRef = useRef(null)
     const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
+    const revealRef = useRef(null)
     const [currentItem, setCurrentItem] = useState<null | number>(null)
     const lastMousePos = useRef({ x: 0, y: 0 })
 
@@ -83,6 +83,7 @@ export default function ContentList ({
             return() => ctx.revert();
         }, component);
     };
+
     window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
@@ -97,8 +98,6 @@ export default function ContentList ({
     const onMouseLeave = () => {
         setCurrentItem(null);
     }
-
-    
 
     const contentImages = items.map((item) => {
         const image = isFilled.image(item.data.hover_image) ? item.data.hover_image : fallbackItemImage
@@ -120,29 +119,30 @@ export default function ContentList ({
     }, [contentImages])
 
   return (
-    <div ref={component}>
-        <ul className="grid border-b border-b-slate-100" onMouseLeave={onMouseLeave}>
-            {items.map((item, index) => (
-                <>
-                    {isFilled.keyText(item.data.title) && (       
+        <div ref={component}>
+            <ul className="grid border-b border-b-slate-100" 
+                onMouseLeave={onMouseLeave}>
+                {items.map((item, index) => (     
+                    <>
+                    {isFilled.keyText(item.data.title) && (
                         <li
-                        key={index}
+                        key={index} 
                         ref={(el) => (itemsRef.current[index] = el)}
-                        onMouseEnter={() => onMouseEnter(index)}
                         className="list-item opacity-0"
+                        onMouseEnter={() => onMouseEnter(index)}
                         >
                             <Link 
                                 href={urlPrefix + "/" + item.uid} 
                                 className="flex flex-col justify-between border-t border-t-slate-100 py-5 text-slate-200 md:flex-row" 
                                 aria-label={item.data.title}
-                            >
+                                >
                             <div className="flex flex-col">
                                 <span className="text-3xl font-bold">
                                     {item.data.title}
                                 </span>
                                 <div className="flex gap-3 text-green-400 text-lg font-bold">
                                     {item.tags.map((tag, index) =>(
-                                    <span key = {index}>
+                                        <span key = {index}>
                                         {tag}
                                     </span>
                                     ))}
@@ -153,18 +153,18 @@ export default function ContentList ({
                             </span>
                             </Link>
                         </li>
-                    )}
-                 </>
-            ))}     
-        </ul>
+                )}
+                </>
+            ))}  
 
         <div 
-            className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
+            className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[420px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
             style={{
                 backgroundImage: currentItem !== null ? `url(${contentImages[currentItem]})` : "",
             }}
             ref={revealRef}
-        >
+            >
         </div>
+        </ul>
     </div>
 )}
