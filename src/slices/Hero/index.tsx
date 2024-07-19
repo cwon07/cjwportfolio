8df58c9cde8b191ace2a197ce5f3f21cae054f40
cwon07 <jumiryoo@gmail.com>
2024-07-18 21:34:59 -0700

    changed the header size of first and last name on Hero slice to h4

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -69,10 +69,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <Shapes />
         <div className="col-start-1 md:row-start-1" data-speed=".2">    
-          <h3 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>  
+          <h4 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.first_name + " " + slice.primary.last_name}>  
             <span className="block text-slate-300">{renderLetters(slice.primary.first_name, "first")}</span>
             <span className="-mt-[.2em] block text-slate-500">{renderLetters(slice.primary.last_name, "last")}</span>   
-          </h3> 
+          </h4> 
           <span className="job-title block bg-gradient-to-tr from-green-500 via-green-200 to-green-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">{slice.primary.tag_line}</span>
         </div>
       </div>
